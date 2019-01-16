c86cc5f9190066e3e3df5eab2161fcd104874a72
Dat Luu <tad.louis2810@gmail.com>
2019-01-16 15:35:16 +0700

    board add event

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -1,34 +1,53 @@
 package Board;
 
+import Board.EventHandle.EventHandle;
 import GameDriver.Driver;
+import MainPackage.MainDriver;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 import java.io.InputStream;
 
+
+
 public class Board {
     private Driver driver;
+    private EventHandle eventHandle;
+    //private ArrayList<Image> images = new ArrayList<>();
 
     private static double scaleSize;
+
     static {
         scaleSize = 45;
     }
 
-    public Board(Driver _driver) {
+    //Add event handler to the board
+    public Board(Driver _driver, EventHandle _eventHandle) {
         driver = _driver;
+        eventHandle = _eventHandle;
+        //Call the method
         loadSquareIMG();
     }
 
     private void loadSquareIMG() {
         //Load Image
-        InputStream input = Board.class.getResourceAsStream("Square.jpg");
-        Image imageSquare = new Image(input);
+        InputStream urlSquareIMG = MainDriver.class.getResourceAsStream("Asset/Square.jpg");
+        Image imageSquare = new Image(urlSquareIMG);
 
         //Fill the screen
         for (int h = 0; h < 14; h++)
             for (int w = 0; w < 28; w++) {
+                //images.add(imageSquare);
+
                 //Image View
                 ImageView imageView = new ImageView(imageSquare);
+
+                double finalW = w * scaleSize;
+                double finalH = h * scaleSize;
+
+                //Set event for the image
+                imageView.setOnMouseClicked((MouseEvent event) -> eventHandle.eventMouseClick(finalW, finalH));
 
                 //Set the scale for x and y
                 imageView.setFitHeight(scaleSize);
@@ -41,9 +60,6 @@ public class Board {
                 //Add image to root
                 driver.getRoot().getChildren().addAll(imageView);
             }
-
-
-
     }
 
 }
